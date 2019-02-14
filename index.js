7df40ebd51eaf1e952eae36d8cee1f4edb22ce95
Daniel Masi <beenhere4hours@gmail.com>
2019-02-14 15:53:07 -0500

    recursion failing in testing

--- index.js
+++ index.js
@@ -5,20 +5,16 @@
  * return {Object}
  */
 exports.mergeObjects = function (object1, object2) {
-   for (var property in object2) {
-      // ES5 will coerce a primitive to an object
-      // so don't trust isEnumerable
-      // and check for object type
-      if (object1.hasOwnProperty(property) &&
-          object2.propertyIsEnumerable(property) &&
-          typeof object2[property] === "object") {
+    for (let property in object2) {
+        // ES5 will coerce a primitive to an object so don't trust isEnumerable and check for object type
+        if (object1.hasOwnProperty(property) && object2.propertyIsEnumerable(property) && typeof object2[property] === "object") {
+            exports.mergeObjects(object1[property], object2[property]);
 
-        mergeObjects(object1[property], object2[property]);
+        } else {
+            object1[property] = object2[property];
 
-      } else {
-        object1[property] = object2[property];
-      }
-  }
+        }
+    }
 
-  return object1;
+    return object1;
 };
