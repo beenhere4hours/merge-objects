1e09f514d3ac61bcf2de0450cd179b94f8c5cee5
Daniel Masi <beenhere4hours@gmail.com>
2019-02-16 11:45:26 -0500

    updated test for variadic recursion

--- test/test.js
+++ test/test.js
@@ -2,9 +2,16 @@ const should = require('chai').should();
 const mergeObjects = require("../index").mergeObjects;
 
 it('should return true if the objects are combined properly', function () {
-    const object1 = {firstName: "John", lastName: "Doe", address: {street: "123 Main St"}};
-    const object2 = {id: "1234567890", address: {state: "OH"}};
-    const expectedResult = {address: {state: "OH", street: "123 Main St"}, firstName: "John", id: "1234567890", lastName: "Doe"};
+    const object1 = {firstName: "John", lastName: "Doe", address: {street: "123 Main St", city: "Youngstown"}};
+    const object2 = {id: "1234567890", address: {state: "OH", zip: 44504}};
+    const object3 = {attributes: {dob: "01-01-76", hair: "brown", eyes: "brown"}};
+    const expectedResult = {
+        address: {street: "123 Main St", city: "Youngstown", state: "OH", zip: 44504},
+        attributes: {dob: "01-01-76", hair: "brown", eyes: "brown"},
+        firstName: "John",
+        lastName: "Doe",
+        id: "1234567890"
+    };
 
-    mergeObjects(object1, object2).should.eql(expectedResult);
+    mergeObjects(object1, object2, object3).should.eql(expectedResult);
 });
