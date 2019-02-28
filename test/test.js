7834b51677bcfd5ce398f0627e8eceb46c1fb3e4
Daniel Masi <beenhere4hours@gmail.com>
2019-02-28 16:00:11 -0500

    added test for deep copy

--- test/test.js
+++ test/test.js
@@ -1,17 +1,29 @@
 const should = require('chai').should();
 const mergeObjects = require("../index").mergeObjects;
 
-it('should return true if the objects are combined properly', function () {
-    const object1 = {firstName: "John", lastName: "Doe", address: {street: "123 Main St", city: "Youngstown"}};
-    const object2 = {id: "1234567890", address: {state: "OH", zip: 44504}};
-    const object3 = {attributes: {dob: "01-01-76", hair: "brown", eyes: "brown"}};
-    const expectedResult = {
-        address: {street: "123 Main St", city: "Youngstown", state: "OH", zip: 44504},
-        attributes: {dob: "01-01-76", hair: "brown", eyes: "brown"},
-        firstName: "John",
-        lastName: "Doe",
-        id: "1234567890"
-    };
+describe('deep recursive copy', function () {
+    it('should create deeply nested object', function () {
+        const object1 = {firstName: "John", lastName: "Doe", address: {street: "123 Main St", city: "Youngstown"}};
+        const object2 = {id: "1234567890", address: {state: "OH", zip: 44504}};
+        const object3 = {attributes: {dob: "01-01-76", hair: "brown", eyes: "brown"}};
+        const expectedResult = {
+            address: {street: "123 Main St", city: "Youngstown", state: "OH", zip: 44504},
+            attributes: {dob: "01-01-76", hair: "brown", eyes: "brown"},
+            firstName: "John",
+            lastName: "Doe",
+            id: "1234567890"
+        };
 
-    mergeObjects(object1, object2, object3).should.eql(expectedResult);
+        mergeObjects(object1, object2, object3).should.eql(expectedResult);
+    });
 });
+
+describe('deep copy', function () {
+    it('should make a copy of an object not a reference to it', function () {
+        const object1 = {firstName: "John", lastName: "Doe", address: {street: "123 Main St", city: "Youngstown"}};
+
+        mergeObjects({}, object1).should.be.eql(object1);
+        mergeObjects({}, object1).should.not.be.equal(object1);
+    });
+});
+
